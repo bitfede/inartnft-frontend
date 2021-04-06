37a16dd5f1fcdb437fea3776d2f6468e92d3daa6
bitfede <f.g.defaveri@gmail.com>
2021-04-06 03:09:53 -0400

    Starts working on the home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
+/*
+ AUTHOR: Federico G. De Faveri
+ DATE: April 6th, 2021
+ PURPOSE: This is the home page of the InArt NFT platform.
+*/
+
+//dependencies
+
+//hooks
+import { useEthers } from '@usedapp/core';
+
+//components
 import Head from 'next/head'
+
+//assets
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
 
-
+  const { activateBrowserWallet } = useEthers();
 
   return (
     <div className={styles.container}>
