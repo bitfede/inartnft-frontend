6722f986019a76f57a08ea99542a621aa6cdd11f
bitfede <f.g.defaveri@gmail.com>
2021-04-21 03:22:34 -0400

    Implements the login functionality on a modal instead of a separate page

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,14 @@
 //dependencies
 import React, { useEffect, useState } from "react";
 import { InjectedConnector } from "@web3-react/injected-connector";
-import { Nav, NavDropdown, Navbar, Modal, Button, ListGroup, Image, Spinner } from "react-bootstrap";
+import { Nav, NavDropdown, Navbar, Modal, Button, ListGroup, Image, Spinner, Form } from "react-bootstrap";
 import httpClient from '../utilities/http-client';
 
 //hooks
 import { useEthers } from "@usedapp/core";
 import { useAuth } from "../hooks/auth";
+import usePersonalSign from "../hooks/usePersonalSign";
+
 
 //assets
 import styles from '../styles/Header.module.css'
@@ -14,16 +16,23 @@ import styles from '../styles/Header.module.css'
 //global variables
 const injected = new InjectedConnector({ supportedChainIds: [1, 4, 1337] });
 
+
+
+
 const Header = () => {
 
     //state
     const [loginState, setLoginState] = useState(null);
     const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
+	const [userData, setUserData] = useState(null);
+	const [newUserEmail, setNewUserEmail] = useState(null);
+	const [newUsername, setNewUsername] = useState(null);
 
     //hooks
-	const { authToken, userId, logout } = useAuth();
+	const { authToken, userId, logout, saveAuth } = useAuth();
 	const { account, activate, activateBrowserWallet, deactivate } = useEthers();
+	const sign = usePersonalSign();
 
     //useEffect functions
 	useEffect(() => {
@@ -35,31 +44,52 @@ const Header = () => {
 	}, [activate]);
 
 	useEffect( async () => {
-		console.log("LOGINSTATE CHANGED")
+		console.log("LOGINSTATE CHANGED", loginState)
 
 		if (!loginState) { return }
 
-		if (loginState.provider === 'metamask' && account) {
+		if (loginState.provider === 'metamask' && account && !userData) {
 			
 			const loginAnswer = await httpClient.post("/User/Login", {
 				address: account,
 			});
 			console.log("API Answer:", loginAnswer);
 	
+			// TODO: wrap in try catch
 			const loginAnswerStatus = loginAnswer.status;
 			// TODO: manage all the statuses here!
 	
 			const loginAnswerData = loginAnswer.data;
 
-			console.log("DATAAAAA", loginAnswerData)
+			console.log("DATAAAAA", loginAnswerData);
+			setIsLoading(false);
+			setUserData(loginAnswerData)
+
+			// if we need to create the user change state and stop execution
+			if (loginAnswerData.errormessage === "CREATE_USER") {
+				console.log("Create a new user!")
+				let newLoginState = {...loginState};
+
+				newLoginState.newUser = true;
+
+				setLoginState(newLoginState);
+
+				return;
+			}
+
+			// user found, ask the user to sign the nonce with metamask
+			if (loginAnswerData.nonce) {
+				signNonceAndAuth(loginAnswerData)
+			}
+
 
 		}
 
 
-	}, [loginState])
+	}, [loginState, account])
 
     //functions
-    const _startLoginFlow = () => {
+    const _handleStartLoginFlow = () => {
         console.log("login!");
         setIsLoginModalOpen(true);
     }
@@ -68,7 +98,6 @@ const Header = () => {
         console.log("Provider >> ", provider, "selected")
         const newLoginState = {
             provider: "metamask",
-            walletConnected: false,
             newUser: null 
         }
 
@@ -76,6 +105,67 @@ const Header = () => {
         setLoginState(newLoginState)
         
     }
+
+	const signNonceAndAuth = async (loginAnswerData) => {
+		const nonce = loginAnswerData.nonce;
+		const sig = await sign(nonce);
+		console.log("SIGNED:", sig);
+
+		// new ajax call // wrap in try..catch
+		const loginAnswer2 = await httpClient.post("/User/Authentication", {
+			...loginAnswerData,
+			sign: sig,
+		});
+		console.log(2, loginAnswer2);
+
+		const loginAnswer2Status = loginAnswer2.status;
+		// TODO manage the statuses
+
+		const loginAnswer2Data = loginAnswer2.data;
+
+		const token = loginAnswer2Data.token.access_token;
+		// maybe save it in localstorage of browser to persist?
+		const userProfile = loginAnswer2Data._user;
+		const profileId = userProfile.id;
+
+		saveAuth(token, profileId);
+
+		setTimeout(function () {
+			setIsLoginModalOpen(false)
+		}, 2000);
+
+	}
+
+	const _handleSubmitRegistration = async () => {
+		console.log("Registering", newUserEmail, newUsername)
+		setIsLoading(true)
+
+		const loginAnswer = await httpClient.post("/User/Login", {
+			address: account,
+			username: newUsername,
+			mail: newUserEmail 
+		});
+		console.log("API Answer:", loginAnswer);
+
+		// TODO: wrap in try catch
+		const loginAnswerStatus = loginAnswer.status;
+		// TODO: manage all the statuses here!
+
+		const loginAnswerData = loginAnswer.data;
+
+		console.log("DATAAAAA", loginAnswerData);
+
+		setIsLoading(false);
+		setUserData(loginAnswerData)
+
+		
+		if (loginAnswerData.nonce) {
+			signNonceAndAuth(loginAnswerData)
+		}
+
+		//catch other cases...
+
+	}
 
     //UI render functions
     const renderLoginModalContent = () => {
@@ -107,8 +197,8 @@ const Header = () => {
             )
         }
 
-		//metamask selected as provider, and wallet is connected to webapp
-		if (loginState.provider === 'metamask' && account) {
+		//metamask selected as provider, and wallet is just connected to webapp
+		if (loginState.provider === 'metamask' && account && !loginState.newUser) {
 			return (
                 <div>
                     <h4>Connect your Metamask wallet</h4>
@@ -116,6 +206,40 @@ const Header = () => {
 					{ isLoading ? (<Spinner className={styles.loadingSpinner} animation="grow" />) : <p>Please sign the message</p>}
                 </div>
             )
+		}
+
+
+		if (loginState.provider === 'metamask' && account && loginState.newUser === true) {
+			
+			if (userData.errormessage !== "CREATE_USER") {
+				console.log("userdata", userData)
+				return (
+					<div>
+						<h4>Register a new account</h4>
+						<p>wallet <strong>{account}</strong></p>
+						{	isLoading ? (<Spinner className={styles.loadingSpinner} animation="grow" />) :
+							(<p>Please sign the message in your metamask window</p>)}
+					</div>
+				)
+			}
+			
+			
+			return (
+				<div>
+					<h4>Register a new account</h4>
+					<p>Wallet ID: <strong>{account}</strong></p>
+					<Form.Group>
+						<Form.Label>Email address</Form.Label>
+						<Form.Control onChange={e => setNewUserEmail(e.target.value)} type="email" placeholder="name@example.com" />
+					</Form.Group>
+					<Form.Group>
+						<Form.Label>Username</Form.Label>
+						<Form.Control onChange={e => setNewUsername(e.target.value)} type="username" placeholder="username" />
+					</Form.Group>
+					{  isLoading ? (  <Button variant="primary" disabled> <Spinner as="span" animation="grow" size="sm" role="status" aria-hidden="true"/>Loading...</Button>) :
+						(<Button onClick={() => _handleSubmitRegistration()}>Log In</Button>)}
+				</div>
+			)			
 		}
 
     }
@@ -174,7 +298,7 @@ const Header = () => {
 							</Nav.Item>
 						) : (
 							<Nav.Item>
-								<Nav.Link className={"hvr-underline-from-left btn"} onClick={() => _startLoginFlow()} eventKey="login">
+								<Nav.Link className={"hvr-underline-from-left btn"} onClick={() => _handleStartLoginFlow()} eventKey="login">
 									Login
 								</Nav.Link>
 							</Nav.Item>
@@ -193,7 +317,7 @@ const Header = () => {
                 
                 <Modal.Header closeButton>
                     <Modal.Title id="contained-modal-title-vcenter">
-                    Login  
+                    LOGIN  
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body className={styles.modalBodyBigContainer}>
