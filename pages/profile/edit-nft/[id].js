95775ec79a4a8e44f682a3396b2a863ca288f8a1
bitfede <f.g.defaveri@gmail.com>
2021-05-26 08:48:57 -0400

    Adds the product info as a props into the edit product page

--- pages/profile/edit-nft/[id].js
+++ pages/profile/edit-nft/[id].js
@@ -27,16 +27,22 @@ import { useAuth } from "../../../hooks/auth";
 // COMPONENT STARTS HERE
 function ProfilePage(props) {
 
+	console.log("PROPS", props)
+	const {product} = props;
+
 	const { authToken } = useAuth();
 	const {account} = useEthers();
 
-	// const [account, setAccount] = useState("");
-	// const [userId, setUserId] = useState(null);
+	const [productObj, setProductObj] = useState(null)
 
 
 	useEffect(async () => {
 		console.log("@ PAGE LOAD!");
-		console.log("ACCOUNT", account)
+		console.log("ACCOUNT", account);
+		console.log("PRODUCT", product);
+
+		setProductObj(product);
+		
 	}, []);
 
 
@@ -47,26 +53,35 @@ function ProfilePage(props) {
 	//render
 	return (
 		<Layout title="Profile">
-			<div id={styles.editArtPageContainer}>
-				<Container>
-                    input form
-                    input form 
-                    etc etc
-				</Container>
-			</div>
+			<Container id={styles.editArtPageContainer}>
+				<div className={"inputsCard"}>
+					
+				</div>
+			</Container>
 		</Layout>
 	);
 }
 
 export async function getStaticProps(context) {
-	console.log("CONTEXT", context);
-
+	console.log("CONTEXT", context.params.id)
+	const productId = context.params.id;
+	const payload = JSON.stringify(productId);
+	const productRes = await httpClient.post("/PublicProduct", payload);
+	//  TODO Gestire gli status HTTP
+	const product = productRes.data;
+	  if (!product) {
+	    return {
+	      notFound: true,
+	    }
+	  }
+	  console.log(product)
 	return {
-		props: { loading: true }, // will be passed to the page component as props
+		props: { product: product }, // will be passed to the page component as props
 	};
 }
 
 export const getStaticPaths = async slug => {
+
 	return {
 		paths: [], //indicates that no page needs be created at build time
 		fallback: "blocking", //indicates the type of fallback
